a9c483d62379bd400e6356a5288ddf28f7b5bd34
veydp <veydp@andrew.cmu.edu>
2025-04-12 19:24:34 -0400

    Update Translate to ASL.py

---
+++ Translate to ASL.py
@@ -0,0 +1,36 @@
+from cmu_graphics import *
+
+image_folder = #Insert the path to your image folder here
+
+def onAppStart(app):
+    app.imageDict = {
+        'A': 'A.png',
+        'B': 'B.png',
+        'C': 'C.png',
+        'D': 'D.png',
+        'E': 'E.png',
+        'F': 'F.png',
+        'G': 'G.png',
+        'H': 'H.png',
+        'I': 'I.png',
+        'J': 'J.png',
+        'K': 'K.png',
+        'L': 'L.png',
+        'M': 'M.png',
+        'N': 'N.png',
+        'O': 'O.png',
+        'P': 'P.png',
+        'Q': 'Q.png',
+        'R': 'R.png',
+        'S': 'S.png',
+        'T': 'T.png',
+        'U': 'U.png',
+        'V': 'V.png',
+        'W': 'W.png',
+        'X': 'X.png',
+        'Y': 'Y.png',
+        'Z': 'Z.png',
+    }
+
+
+cmu_graphics.run()
