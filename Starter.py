0fc2a600331ec69d81341c04505c7667e840a43d
Declan0206 <domelia@andrew.cmu.edu>
2025-04-12 20:53:16 -0400

    Halway UI

--- Starter.py
+++ Starter.py
@@ -1,19 +1,53 @@
 from cmu_graphics import *
 
 def onAppStart(app):
-    app.color = 'white'
+    app.color = 'gray'
+    app.buttonOneX = app.width/2    
+    app.buttonTwoX = app.width/2
+    app.buttonOneY = 125
+    app.buttonTwoY = 275
+    app.insideBoxOne = False
+    app.insideBoxTwo = False
+    app.translateToASL = False
+    app.translateFromASL = False
+
+def onMouseMove(app, mouseX, mouseY):
+    app.insideBoxOne = (app.buttonOneX - 100 < mouseX < app.buttonOneX + 100 and
+                      app.buttonOneY - 50 < mouseY < app.buttonOneY + 50)
+    app.insideBoxTwo = (app.buttonTwoX - 100 < mouseX < app.buttonTwoX + 100 and
+                      app.buttonTwoY - 50 < mouseY < app.buttonTwoY + 50)
 
 def onMousePress(app, mouseX, mouseY):
-    if app.width/2 - 75 < mouseX < app.width/2 + 75 and app.height/2 - 25 < mouseY < app.height/2 + 25:
-        app.translate = True
-    else:
-        app.translate = False
-    pass
+    if (app.buttonOneX - 100 < mouseX < app.buttonOneX + 100 and
+        app.buttonOneY - 50 < mouseY < app.buttonOneY + 50):
+        app.translateToASL = True
+    elif (app.buttonTwoX - 100 < mouseX < app.buttonTwoX + 100 and
+          app.buttonTwoY - 50 < mouseY < app.buttonTwoY + 50):
+        app.translateFromASL = True
+    elif (32.5 < mouseX < 102.5 and 340 < mouseY < 380):
+        app.translateToASL = False
+        app.translateFromASL = False
+
 
 def redrawAll(app):
-    drawRect(app.width/2, app.height/2,150,50, fill = app.color,
-             border='black',align='center')
-    drawLabel('Translate to ASL', app.width/2, app.height/2, font='A', bold = True)
+    if not app.translateToASL and not app.translateFromASL:
+        colorOne = 'black' if app.insideBoxOne else 'gray'
+        colorTwo = 'black' if app.insideBoxTwo else 'gray'
+        drawLabel('ASL Translator', app.width/2, 30, size = 30, bold = True)
+        drawRect(app.buttonOneX, app.buttonOneY, 200, 100, fill = colorOne,
+                border='black',align='center', opacity = 50)
+        drawRect(app.buttonTwoX, app.buttonTwoY, 200, 100, fill = colorTwo,
+                border='black',align='center', opacity = 50)
+        drawLabel('Translate to ASL', app.buttonOneX, app.buttonOneY, bold = True, size = 20)
+        drawLabel('Translate from ASL', app.buttonTwoX, app.buttonTwoY, bold = True, size = 20)
+    elif app.translateToASL:
+        drawLabel('Translate to ASL', app.width/2, 30, size = 30, bold = True)
+        drawRect(70, app.height - 40, 75, 40, fill = 'red', opacity = 75, align = 'center', border = 'black')
+        drawLabel('Back', 70, app.height - 40, size = 20)
+    elif app.translateFromASL:
+        drawLabel('Translate from ASL', app.width/2, 30, size = 30, bold = True)
+        drawRect(70, app.height - 40, 75, 40, fill = 'red', opacity = 75, align = 'center', border = 'black')
+        drawLabel('Back', 70, app.height - 40, size = 20)
 
 def main():
     runApp()
